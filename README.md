1c6a194cba639e12672dc462c3e925df918706b5
segun ola <segunlalive@gmail.com>
2017-03-09 09:00:21 +0100

    added README file

---
+++ README.md
@@ -0,0 +1,16 @@
+# VanillaChat
+
+### A vanilla MVC node.js project to explore web development. No frameworks or
+### external dependencies besides mime which I used because I didn't want to write
+### a mime lookup function myself.
+
+
+It includes a:
+
+* Web server
+
+* Url Router
+
+* A template engine for server-side rendering.
+
+It utilizes the Event Source and Notification APIs in the frontend.
--- static/index.js
+++ static/index.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', function () {
   // At first, let's check if we have permission for notification
   // If not, let's ask for it
-  if (window.Notification && Notification.permission !== "granted") {
+    if (window.Notification && Notification.permission !== "granted") {
       Notification.requestPermission().then((result)=>{
         if (result === 'denied') {
           console.log('Permission wasn\'t granted. Allow a retry.');
@@ -17,149 +17,152 @@ window.addEventListener('load', function () {
         'show up here'});
         closeNotification(newMessage);
       });
-  }
-});
-
-
-function messageNotification (data) {
-    let options = {
-        body: `${data.name}: ${data.message}`
     }
-    if (!window.Notification) {
-        return;
-    } else if (Notification.permission === 'granted') {
-        let newMessage = new Notification('Vanilla', options);
-        let vibration = window.navigator.vibrate([300, 100, 300]);
-        closeNotification(newMessage);
-    } else if (Notification.permission !== 'denied') {
-        Notification.requestPermission().then((result)=>{
-            // If the user accepts, let's create a notification
-            if (permission === "granted") {
-                let newMessage = new Notification('Vanilla', options);
-                closeNotification(newMessage);
-            }
-        });
-    }
-}
-
-function closeNotification(message) {
-    setTimeout(message.close.bind(message), 4000);
-}
+// });
 
 
-var message_form = document.querySelector('.message_form');
-message_form.addEventListener('submit', handleMessage, false);
-
-
-function elt(tag, attributes) {
-    var node = document.createElement(tag);
-    if (attributes){
-        for (let attr in attributes) {
-            if (attributes.hasOwnProperty(attr))
-                node.setAttribute(attr, attributes[attr])
+    function messageNotification (data) {
+        let options = {
+            body: `${data.name}: ${data.message}`
+        }
+        if (!window.Notification) {
+            return;
+        } else if (Notification.permission === 'granted') {
+            let newMessage = new Notification('Vanilla', options);
+            let vibration = window.navigator.vibrate([300, 100, 300]);
+            closeNotification(newMessage);
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission().then((result)=>{
+                // If the user accepts, let's create a notification
+                if (permission === "granted") {
+                    let newMessage = new Notification('Vanilla', options);
+                    closeNotification(newMessage);
+                }
+            });
         }
     }
-    for (var i = 2; i < arguments.length; i++) {
-        var child = arguments[i];
-        if (typeof child == "string")
-          child = document.createTextNode(child);
-        node.appendChild(child);
-      }
-    return node;
-}
 
-function setName() {
-    if (!localStorage.name) {
-        let name = prompt('Please enter your name');
-        localStorage.setItem('name', name);
+    function closeNotification(message) {
+        setTimeout(message.close.bind(message), 4000);
     }
-}
 
-function handleMessage(event) {
-    let name;
-    if (!localStorage.name) {
-        name = prompt('Please enter your name');
-        localStorage.setItem('name', name);
+    var message_form = document.querySelector('.message_form');
+    message_form.addEventListener('submit', handleMessage, false);
+
+
+    function setName() {
+        if (!localStorage.name) {
+            let name = prompt('Please enter your name');
+            localStorage.setItem('name', name);
+        }
+    }
+
+    function handleMessage(event) {
+        let name;
+        if (!localStorage.name) {
+            name = prompt('Please enter your name');
+            localStorage.setItem('name', name);
+        } else {
+            name = localStorage.getItem('name')
+        }
+        var inputs = message_form.elements;
+        var message = inputs['message'].value.trim();
+        var payload = {
+            name: name,
+            message: message,
+        };
+        inputs['message'].value = '';
+        inputs['message'].focus();
+
+        ajaxRequest('/rooms/private', 'POST', payload);
+        event.preventDefault();
+    }
+
+
+    function ajaxRequest(url, method, data) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+        if (data) {
+            xhr.setRequestHeader("Content-type", "application/json");
+            var payload = JSON.stringify(data);
+        }
+        xhr.send(payload);
+    }
+
+
+
+    // Server Response
+
+    function elt(tag, attributes) {
+        var node = document.createElement(tag);
+        if (attributes){
+            for (let attr in attributes) {
+                if (attributes.hasOwnProperty(attr))
+                    node.setAttribute(attr, attributes[attr])
+            }
+        }
+        for (var i = 2; i < arguments.length; i++) {
+            var child = arguments[i];
+            if (typeof child == "string")
+              child = document.createTextNode(child);
+            node.appendChild(child);
+          }
+        return node;
+    }
+
+
+    function updateDOM(data) {
+        var div = elt('div', {class: 'messagePanel'});
+        var namePara = elt('p', {class: 'username'});
+        namePara.textContent = data.name + ':';
+        var time = elt('span', {class: 'time'})
+        time.textContent = getTime(data.date);
+        var messagePara = elt('p', {class: 'messageBody'});
+        messagePara.textContent = data.message;
+
+        div.appendChild(time);
+        div.appendChild(namePara);
+        div.appendChild(messagePara);
+
+        var messagesDiv = document.getElementById('messages');
+        messagesDiv.appendChild(div);
+        scrollUp(messagesDiv);
+        messageNotification(data);
+    }
+
+
+    function getTime(date) {
+        var date = new Date(date);
+        var hours = (date.getHours() > 9)
+                    ? date.getHours()
+                    : '0' + date.getHours();
+        var minutes = (date.getMinutes() > 9)
+                        ? date.getMinutes()
+                        : '0' + date.getMinutes();
+        return hours + ':' + minutes;
+    }
+
+    function scrollUp (el) {
+        if (!(el.scrollHeight - el.scrollTop === el.clientHeight)) {
+            window.scroll(0, (el.scrollHeight));
+        }
+    }
+
+
+
+    if (window.EventSource) {
+        var source = new EventSource('/rooms/private');
+        source.addEventListener('message', sseHandler, false);
     } else {
-        name = localStorage.getItem('name')
+        alert("Your browser isn't supported. Try Chrome or Firefox");
     }
-    var inputs = message_form.elements;
-    var message = inputs['message'].value;
-    var payload = {
-        name: name,
-        message: message,
-    };
-    inputs['message'].value = '';
-    inputs['message'].focus();
 
-    ajaxRequest('/rooms/private', 'POST', payload);
-    event.preventDefault();
-}
-
-
-function ajaxRequest(url, method, data) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url, true);
-    if (data) {
-        xhr.setRequestHeader("Content-type", "application/json");
-        var payload = JSON.stringify(data);
+    function sseHandler (event) {
+        try {
+            let responseData = JSON.parse(event.data);
+            updateDOM(responseData);
+        } catch (e) {
+            console.error(e);
+        }
     }
-    xhr.send(payload);
-}
-
-
-
-// Server Response
-
-function getTime(date) {
-    var date = new Date(date);
-    var hours = (date.getHours() > 9)
-                ? date.getHours()
-                : '0' + date.getHours();
-    var minutes = (date.getMinutes() > 9)
-                    ? date.getMinutes()
-                    : '0' + date.getMinutes();
-    return hours + ':' + minutes;
-}
-
-function scrollUp (el) {
-    if (!(el.scrollHeight - el.scrollTop === el.clientHeight)) {
-        window.scroll(0, (el.scrollHeight));
-    }
-}
-
-function updateDOM(data) {
-    var div = elt('div', {class: 'messagePanel'});
-    var namePara = elt('p', {class: 'username'});
-    namePara.textContent = data.name + ':';
-    var time = elt('span', {class: 'time'})
-    time.textContent = getTime(data.date);
-    var messagePara = elt('p', {class: 'messageBody'});
-    messagePara.textContent = data.message;
-
-    div.appendChild(time);
-    div.appendChild(namePara);
-    div.appendChild(messagePara);
-
-    var messagesDiv = document.getElementById('messages');
-    messagesDiv.appendChild(div);
-    scrollUp(messagesDiv);
-    messageNotification(data);
-}
-
-
-if (window.EventSource) {
-    var source = new EventSource('/rooms/private');
-    source.addEventListener('message', sseHandler, false);
-} else {
-    alert("Your browser isn't supported. Try Chrome or Firefox");
-}
-
-function sseHandler (event) {
-    try {
-        let responseData = JSON.parse(event.data);
-        updateDOM(responseData);
-    } catch (e) {
-        console.error(e);
-    }
-}
+});
